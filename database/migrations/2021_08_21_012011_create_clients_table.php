63b4a5401e32d4fe31f66ab1de520cb5a00d9302
Galmy Louis <louisgalmy88@gmail.com>
2021-08-21 18:50:34 -0400

    Do clients table

---
+++ database/migrations/2021_08_21_012011_create_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',200);
+            $table->string('address',200)->nullable();
+            $table->integer('number')->nullable();
+            $table->float('balance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
